03b69e3fa9237b42453ba41f7d4faf87e7b1c021
007 <dr007cc@gmail.com>
2023-04-29 01:50:09 +0330

    fix: move the queries to a new file

---
+++ demeter/nft/query.py
@@ -0,0 +1,173 @@
+import logging
+
+from httpx import Response as _R
+from httpx import post as _POST
+
+_display_url = 'https://hasura2.foundation.app/v1/graphql'
+_display_query = '''
+query ArtworkActivity(
+    $get_art: Boolean!, $get_actor: Boolean!,
+    $addr: String, $token_id: numeric, $actor_pk: String
+) {
+  art: artwork(
+    limit: 1,
+    where: {
+      tokenId: { _eq: $token_id }
+      contractAddress: { _ilike: $addr }
+      deletedAt: { _is_null: true }
+      hiddenAt: { _is_null: true }
+    }
+  ) @include(if: $get_art) {
+    contractAddress
+    id
+    tags
+    name
+
+    assetScheme
+    assetHost
+    assetPath
+    duration
+    mimeType
+
+    collection {
+      name
+      slug
+      maxTokenId
+    }
+    creator: user {
+      ...UserFragment
+    }
+    owner {
+      ...UserFragment
+    }
+  }
+
+  actor: user(
+    limit: 1,
+    where: { publicKey: { _ilike: $actor_pk } }
+  ) @include(if: $get_actor) {
+    ...UserFragment
+  }
+}
+
+fragment UserFragment on user {
+  name
+  username
+  publicKey
+  links
+  twitter: socialVerifications(
+    where: { isValid: { _eq: true }, service: { _eq: "TWITTER" } }
+    limit: 1
+  ) {
+    username
+  }
+}
+'''
+
+
+_event_url = 'https://api.thegraph.com/subgraphs/name/f8n/fnd'
+_event_query = '''
+query Events($min_price: String!, $date: Int!){
+  events: nftHistories(
+    orderBy: date
+    orderDirection: asc
+    where: {
+        amountInETH_gte: $min_price,
+        event_in: [
+            Sold, Bid,
+            OfferAccepted, OfferMade,
+            PrivateSale, BuyPriceAccepted
+        ],
+        date_gte: $date,
+    }
+  ) {
+    amountInETH
+    date
+    event
+    transactionHash
+
+    nft {
+      id
+    }
+
+    offer {
+      buyer {
+        id
+      }
+    }
+
+    privateSale {
+      buyer {
+        id
+      }
+    }
+
+    auction {
+      highestBid {
+        bidder {
+          id
+        }
+        bidThisOutbid {
+          amountInETH
+          bidder {
+            id
+          }
+        }
+      }
+    }
+
+    buyNow {
+      buyer {
+        id
+      }
+    }
+
+  }
+}
+'''
+
+
+def _graghql(url: str, query: str, variables: dict = {}) -> _R:
+    result = _POST(url, json={
+        'query': query,
+        'variables': variables
+    }, timeout=60)
+
+    if result.status_code == 524:
+        logging.error('server timeoit 524')
+
+    elif result.status_code != 200:
+        logging.error((
+            f'error getting events {result.status_code}'
+            f'\n\n{result.text}'
+        ))
+
+    return result
+
+
+def get_events_raw(date, min_price) -> None | list[dict]:
+    try:
+        return _graghql(_event_url, _event_query, {
+            'date': int(date),
+            'min_price': str(min_price)
+        }).json()['data']['events']
+    except Exception as e:
+        logging.error('error getting events.')
+        logging.exception(e)
+
+
+def get_display_raw(addr=None, tid=None, actor_pk=None) -> None | dict:
+    if not ((addr and tid) or actor_pk):
+        return None
+
+    try:
+        return _graghql(_display_url, _display_query, {
+            'get_art': addr and (tid is not None),
+            'get_actor': bool(actor_pk),
+            'addr': str(addr),
+            'token_id': int(tid),
+            'actor_pk': str(actor_pk)
+        }).json()['data']
+    except Exception as e:
+        logging.error('error getting display.')
+        logging.exception(e)
