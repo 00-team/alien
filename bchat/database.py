5c6d92d44a929e164d9f7d3ee7c66b763152ad22
007 <dr007cc@gmail.com>
2023-05-07 18:46:01 +0330

    fix: update database

--- bchat/database.py
+++ bchat/database.py
@@ -1,46 +1,15 @@
 
-import logging
-from enum import Enum, auto
 
 from databases import Database
-from pydantic import BaseModel
+from models import UserModel, Users
 from settings import DATABASE_URL
-from sqlalchemy import Column, Integer, MetaData, String, insert, select, text
+from sqlalchemy import MetaData, insert, select
 from sqlalchemy.ext.declarative import declarative_base
 
 metadata = MetaData()
 database = Database(DATABASE_URL)
 
-Base = declarative_base(metadata=metadata)
-
-
-class Users(Base):
-    __tablename__ = 'users'
-
-    row_id = Column(Integer, primary_key=True, autoincrement=True, index=True)
-    user_id = Column(Integer, unique=True, index=True, nullable=False)
-    name = Column(String, nullable=False)
-    gender = Column(Integer, server_default=text('0'))
-
-
-class Genders(int, Enum):
-    unknown = auto()
-    boy = auto()
-    girl = auto()
-
-
-GENDER_DISPLAY = {
-    Genders.unknown: 'نامعلوم',
-    Genders.boy: 'پسر',
-    Genders.girl: 'دختر'
-}
-
-
-class UserModel(BaseModel):
-    user_id: int
-    gender: int
-    name: str
-    code: int
+BaseTable = declarative_base(metadata=metadata)
 
 
 async def add_user(user_id: int, name: str):
@@ -51,7 +20,7 @@ async def add_user(user_id: int, name: str):
     return await database.execute(query)
 
 
-async def get_user(user_id: int = None, code: int = None):
+async def get_user(user_id: int = None, code: int = None) -> None | UserModel:
     if not (user_id or code):
         return None
 
@@ -60,7 +29,8 @@ async def get_user(user_id: int = None, code: int = None):
     else:
         query = select(Users).where(Users.code == code)
 
-    d = await database.fetch_one(query)
-    logging.info(UserModel(**d))
-    print(type(d), dir(d), d)
-    return d
+    result = await database.fetch_one(query)
+    if not result:
+        return None
+
+    return UserModel(**result)
