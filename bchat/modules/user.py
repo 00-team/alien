619d56fe58f66a90f7f025b9498b6be8137e2aad
007 <dr007cc@gmail.com>
2023-05-07 21:03:44 +0330

    feat: init user_profile

--- bchat/modules/user.py
+++ bchat/modules/user.py
@@ -1,25 +1,49 @@
 
+
 import logging
 
-from database import add_user, get_user
+from dependencies import require_user_data
+from models import GENDER_DISPLAY, UserModel
 from settings import config
 from telegram import Update
 from telegram.ext import ContextTypes
 from utils import toggle_code
 
+Ctx = ContextTypes.DEFAULT_TYPE
 
-async def user_link(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
-    user = update.message.from_user
-    user_data = await get_user(user.id)
 
-    if user_data is None:
-        code = toggle_code(int(await add_user(user.id, user.full_name)))
-    else:
-        code = toggle_code(user_data.row_id)
-
+def get_link(code):
     bot_username = config['BOT']['username']
+    return f't.me/{bot_username}?start={code}'
+
+
+@require_user_data
+async def user_link(update: Update, ctx: Ctx, user_data: UserModel):
+    code = toggle_code(user_data.row_id)
+
+    await update.message.reply_text('your link\n' + get_link(code))
+
+
+@require_user_data
+async def user_info(update: Update, ctx: Ctx, user_data: UserModel):
+    code = toggle_code(user_data.row_id)
 
     await update.message.reply_text(
-        'your link\n'
-        f't.me/@{bot_username}?start={code}'
+        f'your name: {user_data.name}'
+        f'your link: {get_link(code)}'
+        f'your gender: {GENDER_DISPLAY[user_data.gender]}'
     )
+
+
+@require_user_data
+async def user_profile(update: Update, ctx: Ctx, user_data: UserModel):
+    user = update.effective_user
+    pictures = await user.get_profile_photos(limit=1)
+
+    logging.info(
+        f'{pictures.total_count}'
+    )
+
+    logging.info(pictures)
+
+    await update.message.reply_photo(pictures.photos[0][0].file_id, 'GG')
