cc559e9d77aaac35e55311e8d450f4bda3c0b19d
007 <dr007cc@gmail.com>
2023-05-07 18:46:45 +0330

    feat: add user module

--- bchat/modules/user.py
+++ bchat/modules/user.py
@@ -1,5 +1,7 @@
 
-from database import get_user
+import logging
+
+from database import add_user, get_user
 from telegram import Update
 from telegram.ext import ContextTypes
 from utils import toggle_code
@@ -7,8 +9,16 @@ from utils import toggle_code
 
 async def user_link(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
     user = update.message.from_user
-    user_data = get_user(user.id)
+    user_data = await get_user(user.id)
+
+    if user_data is None:
+        code = toggle_code(int(await add_user(user.id, user.full_name)))
+    else:
+        code = toggle_code(user_data.row_id)
+
+    logging.info(ctx.bot)
+    logging.info(f'code: {code}')
 
     await update.message.reply_text(
-        ''
+        't.me/'
     )
