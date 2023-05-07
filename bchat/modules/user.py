720cf10027100da6b092ed281c7e7b5b789b9d76
007 <dr007cc@gmail.com>
2023-05-07 22:58:09 +0330

    feat(bchat): add user profile

--- bchat/modules/user.py
+++ bchat/modules/user.py
@@ -1,11 +1,9 @@
 
 
-import logging
-
 from dependencies import require_user_data
 from models import GENDER_DISPLAY, UserModel
 from settings import config
-from telegram import Update
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import ContextTypes
 from utils import toggle_code
 
@@ -20,30 +18,37 @@ def get_link(code):
 @require_user_data
 async def user_link(update: Update, ctx: Ctx, user_data: UserModel):
     code = toggle_code(user_data.row_id)
-
     await update.message.reply_text('your link\n' + get_link(code))
 
 
 @require_user_data
-async def user_info(update: Update, ctx: Ctx, user_data: UserModel):
-    code = toggle_code(user_data.row_id)
-
-    await update.message.reply_text(
-        f'your name: {user_data.name}'
-        f'your link: {get_link(code)}'
-        f'your gender: {GENDER_DISPLAY[user_data.gender]}'
-    )
-
-
-@require_user_data
 async def user_profile(update: Update, ctx: Ctx, user_data: UserModel):
+    code = toggle_code(user_data.row_id)
     user = update.effective_user
     pictures = await user.get_profile_photos(limit=1)
 
-    logging.info(
-        f'{pictures.total_count}'
+    file_id = config['default_profile_picture']
+
+    if pictures.total_count > 0:
+        file_id = pictures.photos[0][0].file_id
+
+    await update.message.reply_photo(
+        file_id,
+        (
+            f'name: {user_data.name}\n'
+            f'gender: {GENDER_DISPLAY[user_data.gender]}\n'
+            f'age: {user_data.age}\n\n'
+            f'link: {get_link(code)}\n\n'
+        ),
+        reply_markup=InlineKeyboardMarkup([[
+            InlineKeyboardButton(
+                'Edit Profile', callback_data='edit_profile'
+            )
+        ]])
     )
 
-    logging.info(pictures)
 
-    await update.message.reply_photo(pictures.photos[0][0].file_id, 'GG')
+# @require_user_data
+# async def user_edit_profile(update: Update, ctx: Ctx, user_data: UserModel):
+#     await update.message.
+#     return ''
