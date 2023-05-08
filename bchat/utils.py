58b6e2e0bce40eabab992dd0be941cd6894bad36
007 <dr007cc@gmail.com>
2023-05-09 00:15:47 +0330

    fix(bchat): add error handling for toggle_code

--- bchat/utils.py
+++ bchat/utils.py
@@ -1,5 +1,6 @@
 
 import base64
+import logging
 import sys
 
 from settings import BYTE_ORDER
@@ -9,10 +10,16 @@ from gshare import DbDict
 config = DbDict(sys.argv[1], load=True)
 
 
-def toggle_code(code: int | str) -> str | int:
-    if isinstance(code, int):
-        b = code.to_bytes(6, byteorder=BYTE_ORDER)
-        return base64.b64encode(b).decode()
-    else:
-        b = base64.b64decode(code.encode())
-        return int.from_bytes(b, byteorder=BYTE_ORDER)
+def toggle_code(code: int | str) -> str | int | None:
+    try:
+        if isinstance(code, int):
+            b = code.to_bytes(6, byteorder=BYTE_ORDER)
+            return base64.b64encode(b).decode()
+        else:
+            b = base64.b64decode(code.encode())
+            return int.from_bytes(b, byteorder=BYTE_ORDER)
+    except Exception as e:
+        logging.error('toggle code error:')
+        logging.exception(e)
+
+    return None
