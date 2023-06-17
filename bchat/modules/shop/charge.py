e2e2f27aa3e58e2362314d988d96fcc2f21290d1
007 <dr007cc@gmail.com>
2023-06-17 16:08:31 +0330

    fix: update 1

--- bchat/modules/shop/charge.py
+++ bchat/modules/shop/charge.py
@@ -84,7 +84,7 @@ async def buy_phone_charge(update: Update, ctx: Ctx, state: UserModel):
         user_id=user.id,
         score=price,
         reason=f'شارژ {charge} هزار تومانی ' + CHARGE_PTC[ptc],
-        item=ItemType.charge,
+        item_type=ItemType.charge,
         data={'charge': charge, 'ptc': ptc}
     )
     await user_update(
