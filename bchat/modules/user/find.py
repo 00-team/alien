d8f0fed536d1c16b1b90cb1f0c44420ffc1161bf
007 <dr007cc@gmail.com>
2025-05-08 13:21:08 +0330

    fix: find

--- bchat/modules/user/find.py
+++ bchat/modules/user/find.py
@@ -41,7 +41,7 @@ async def find_user(update: Update, ctx: Ctx, state: UserModel):
 
     for e in msg.entities:
         if e.type == MessageEntity.MENTION:
-            target_username = msg.text[e.offset+1:e.offset+e.length]
+            target_username = msg.text[e.offset+1:e.offset+e.length].lower()
             break
 
     if msg.forward_from and not msg.forward_from.is_bot:
